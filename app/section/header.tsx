85816f974ed944c532ffa99a2bb3d89e288d5f75
Laetitia Ashry <ashry.laetitia@gmail.com>
2024-05-30 13:40:19 +0200

    bug fix wave

--- app/section/header.tsx
+++ app/section/header.tsx
@@ -28,14 +28,14 @@ export default function Header() {
 
         <section className={"header flex flex-col items-center relative left-0"} id={"up"}>
 
-            <div className={"md:mt-64 mt-48 px-2 md:px-0"}>
+            <div className={"md:mt-64 mt-52 px-2 md:px-0"}>
                 <h1 className={"font-extrabold text-7xl text-center"}>Ashry
                     Laetitia *</h1>
                 <h2 className={"text-center text-4xl font-medium"}>Développeuse web <span className={"undeline"}>front
                     end</span></h2>
             </div>
 
-            <article className={"flex md:gap-8 gap-3 justify-center md:mt-36 mt-20 quickPres"}>
+            <article className={"flex md:gap-8 gap-3 justify-center md:mt-36 mt-20 quickPres mb-20 md:mb-0"}>
                 {quickPres.map((item, index) => (
                     <div key={`${index}`} className={"flex flex-col gap-5"}>
                             <FontAwesomeIcon icon={item.icon} className={"mr-2 text-foreground/70 text-2xl"} />
