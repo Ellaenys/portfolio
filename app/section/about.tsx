2efede80d758700ae4b710308897c0bb52e1fb52
Laetitia Ashry <ashry.laetitia@gmail.com>
2024-05-31 12:35:28 +0200

    ajout cv

--- app/section/about.tsx
+++ app/section/about.tsx
@@ -1,9 +1,15 @@
-import {Image} from "@nextui-org/react";
+import {
+    Button,
+    Image,
+    Link
+} from "@nextui-org/react";
+import React from "react";
 export default function About() {
 
     const about = {
         picture: "/portrait.png",
         title: "A propos de moi",
+        cv: "/cv.pdf",
         content: "Designeuse pour de nombreuses communautés en ligne durant plus de dix ans, je suis depuis devenue développeuse web front-end. De mes appétences en graphisme à mes compétences en développement, je suis en mesure d'élaborer des applications web, de leur maquette jusqu'au déploiement.   "
     }
 
@@ -12,9 +18,24 @@ export default function About() {
         <section className={"wave-about"}>
             <div className={"about flex flex-wrap gap-10 md:justify-center md:items-center container my-10 w-4/5"} id={"about"}>
 
-                <article className={"m-auto px-3 md:w-1/2"}>
+                <article
+                    className={"m-auto px-3 md:w-1/2"}>
                     <h4 className={"font-bold text-4xl"}>{about.title}</h4>
                     <p className={"md:p-7 pb-8 pt-3"}>{about.content}</p>
+                    <div className={"flex justify-end mt-3 btn-project"}>
+                        <Button
+                            href={about.cv}
+                            target={"_blank"}
+                            as={Link}
+                            size={"sm"}
+                            showAnchorIcon
+                            variant="flat"
+                            className={"font-bold text-foreground/80 uppercase"}
+                        >
+                            Accéder à mon cv
+
+                        </Button>
+                    </div>
                 </article>
                 <aside>
                     <Image
